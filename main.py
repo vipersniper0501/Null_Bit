ae53b971b8e6298d6eb14cd5a307a0eef174436c
ViperSniper0501 <michaelabrenner@gmail.com>
2022-02-16 18:24:07 +0100

    updated ui and main.py

--- main.py
+++ main.py
@@ -16,14 +16,48 @@ class NullBitMainWindow(QMainWindow, Ui_MainWindow):
         self.setFixedSize(1072, 780)
         self.NullBit_Assign_Functions()
 
-    def Change_Mode(self):
+
+    def EncryptXOR(self):
         pass
+
+    def DecryptXOR(self):
+        pass
+
+    def Run(self):
+        currentType = self.Type_Selection.currentText()
+        if (self.Mode_Selection.currentText() == "Encrypt"):
+            if (currentType == "XOR Cipher"):
+                self.EncryptXOR()
+        else:
+            if (currentType == "XOR Cipher"):
+                self.DecryptXOR()
+
+    def Change_Mode(self):
+        currentType = self.Type_Selection.currentText()
+
+        if (self.Mode_Selection.currentText() == "Encrypt"):
+            if (currentType == "XOR Cipher"):
+                self.Load_Key_Button.setDisabled(True)
+            self.Title1.setText("Encrypt Text (Input):")
+            self.Title2.setText("Encoded Text (Output):")
+            self.Key_Label.setText("Encryption Key:")
+        else:
+            self.Title1.setText("Decrypt Text (Input):")
+            self.Title2.setText("Decoded Text (Output):")
+            self.Key_Label.setText("Decryption Key:")
+            if (currentType == "XOR Cipher"):
+                self.Load_Key_Button.setDisabled(False)
 
     def Clear_All(self):
         pass
 
     def NullBit_Assign_Functions(self):
 
+        self.Change_Mode()
+
+
+        self.Execute.clicked.connect(self.Run)
+        self.Mode_Selection.currentIndexChanged.connect(self.Change_Mode)
         self.Clear_Button.clicked.connect(self.Clear_All)
 
 
